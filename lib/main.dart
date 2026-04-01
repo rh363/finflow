34a18edb5051705eba5506e9611464141ce87288
alex massaroni <alex.m@seeweb.it>
2026-04-01 11:58:06 +0200

    Add db connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:finflow_app/app.dart';
+import 'package:finflow_app/core/database/app_database.dart';
 import 'package:flutter/material.dart';
 
+import 'core/database/connection.dart';
+
 void main() {
+  final db = AppDatabase(openConnection());
   runApp(const FinFlowApp());
   // runApp(const FinFlowAppGallery());
 }
